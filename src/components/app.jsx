5768cb51e3b2d727449b4e4019b5cab5edda48f5
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-04 17:58:59 -0500

    Initial commit, added component folder and navbar

---
+++ src/components/app.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar/Navbar';
+export class App extends Component {
+    constructor(props)
+    {   
+        super(props)
+        this.state = 
+        { 
+            
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        )
+    }
+    }
+
+export default App
