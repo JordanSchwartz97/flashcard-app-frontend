b4b9d101305d59691fc761c0d6cb2da29bf43f85
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-05 18:42:42 -0500

    Added conditional rendering and viewdeck now properly maps through the list of decks and prints them to the webpage.

--- src/components/ViewDeck/Viewdeck.js
+++ src/components/ViewDeck/Viewdeck.js
@@ -1,9 +1,23 @@
-// import React from 'react';
-
-// export default function Deck(props) {
-//     {props.deck.map}((item) => {
-//     return (
-//         <div>
-//             <h1>{item.name}</h1>
-//         </div>
-//     )})};
+import React from 'react';
+import './ViewDeck.css';
+export default function ViewDeck(props) {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th id="name" className="name">Name</th>
+                    <th id="description" className="description">Description</th>
+                </tr>
+            </table>
+            {props.deck.map((item) => {
+                return(
+            <table>
+                <tr>
+                    <td id="name" className="name">{item.name}</td>
+                    <td id="description" className="description">{item.description}</td>
+                </tr>
+            </table>
+                )})}
+        </div>
+    )
+}
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,35 +1,52 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar/Navbar';
-import axios from 'axios';
-import './app.css';
-import Deck from './ViewDeck/Viewdeck';
+import React, { Component } from "react";
+import Navbar from "./Navbar/Navbar";
+import axios from "axios";
+import "./app.css";
+import ViewDeck from "./ViewDeck/ViewDeck.js";
+import e from "cors";
 
-export class App extends Component {
-    constructor(props)
-    {   
-        super(props)
-        this.state = 
-        { 
-          deck: []
-        }
-    }
-    componentDidMount(){
-        
-    };
-    async getAllDecks(){
-        axios.get('http://localhost:5000/api/collections/cardDeck')
-        .then(response => this.setState({
-            deck: response.data
-        }));  
-    }
-    render() {
-        return (
-            <div>
-                <Navbar/>
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			deck: [],
+		};
+	}
 
-            </div>
-        )
-    }
-    }
+	componentDidMount() {
+		this.getAllDecks();
+		console.log("component did mount");
+	}
 
-export default App
+	async getAllDecks() {
+		try {
+			let response = await axios.get(
+				"http://localhost:5000/api/collections/cardDeck"
+			);
+			console.log(response.data);
+			this.setState({
+				deck: response.data,
+			});
+		} catch (ex) {
+			console.log(ex);
+		}
+	}
+
+	render() {
+		let count = this.state.deck.length;
+		let isLoaded = false;
+		if (count > 0) {
+			isLoaded = true;
+		}
+
+		return (
+			<div>
+				<Navbar />
+				{isLoaded ? <h1>{count}</h1> : <h1>Loading...</h1>}
+				<ViewDeck deck={this.state.deck} />
+			</div>
+		);
+	}
+}
+
+export default App;
