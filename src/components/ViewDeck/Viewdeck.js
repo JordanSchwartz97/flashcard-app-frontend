28e11e74f72b697725f0876d7aeae6d497ffd6d9
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-05 01:52:05 -0500

    styled navbar deleted sidebar

---
+++ src/components/ViewDeck/Viewdeck.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+// export default function Deck(props) {
+//     {props.deck.map}((item) => {
+//     return (
+//         <div>
+//             <h1>{item.name}</h1>
+//         </div>
+//     )})};
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Navbar from './Navbar/Navbar';
+import axios from 'axios';
+import './app.css';
+import Deck from './ViewDeck/Viewdeck';
+
 export class App extends Component {
     constructor(props)
     {   
         super(props)
         this.state = 
         { 
-            
+          deck: []
         }
+    }
+    componentDidMount(){
+        
+    };
+    async getAllDecks(){
+        axios.get('http://localhost:5000/api/collections/cardDeck')
+        .then(response => this.setState({
+            deck: response.data
+        }));  
     }
     render() {
         return (
             <div>
                 <Navbar/>
+
             </div>
         )
     }
